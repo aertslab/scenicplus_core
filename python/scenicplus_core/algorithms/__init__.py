dc9e62244f6b42a4e744b1757b01bd04d8d64ef1
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-03-11 21:48:45 +0100

    Expose sort related functions to python.
    Expose sort related functions to python.

--- python/scenicplus_core/algorithms/__init__.py
+++ python/scenicplus_core/algorithms/__init__.py
@@ -1,4 +1,5 @@
 from scenicplus_core.algorithms.arg_sort import arg_sort
+from scenicplus_core.algorithms.sort import sort
 from scenicplus_core.scenicplus_core import algorithms as spc_algorithms
 
 norm_sf = spc_algorithms.norm_sf
@@ -6,4 +7,5 @@ norm_sf = spc_algorithms.norm_sf
 __all__ = [
     "arg_sort",
     "norm_sf",
+    "sort",
 ]
---
+++ python/scenicplus_core/algorithms/sort.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import numpy as np
+
+from scenicplus_core.scenicplus_core import algorithms as spc_algorithms
+
+dtype_to_sort_func = {
+    np.int8: spc_algorithms.sort_1d_i8,
+    np.int16: spc_algorithms.sort_1d_i16,
+    np.int32: spc_algorithms.sort_1d_i32,
+    np.int64: spc_algorithms.sort_1d_i64,
+    np.uint8: spc_algorithms.sort_1d_u8,
+    np.uint16: spc_algorithms.sort_1d_u16,
+    np.uint32: spc_algorithms.sort_1d_u32,
+    np.uint64: spc_algorithms.sort_1d_u64,
+    np.float32: spc_algorithms.sort_1d_f32,
+    np.float64: spc_algorithms.sort_1d_f64,
+}
+
+
+def sort(arr: np.ndarray) -> np.ndarray:
+    """
+    Sort an 1D array.
+
+    Parameters
+    ----------
+    arr
+        1D continuous numpy array.
+
+    """
+    if not isinstance(arr, np.ndarray):
+        raise ValueError("Not a numpy array.")
+    if arr.ndim != 1:
+        raise ValueError("Not a 1D numpy array.")
+
+    sort_func = dtype_to_sort_func.get(arr.dtype.type)
+
+    if sort_func:
+        return sort_func(np.ascontiguousarray(arr))
+
+    raise ValueError(
+        f'Unsupported dtype "{arr.dtype}". Only np.int8-64, np.uint8-64 and np.float32-64 are supported.'
+    )
