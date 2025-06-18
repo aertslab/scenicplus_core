47cdabfa1cf0cae6024345c8aec963a68eb0b832
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-06-18 13:52:03 +0200

    Expose `rank_sums` and `rank_sums_2d` directly in the python part of the module.
    Expose `rank_sums` and `rank_sums_2d` directly in the python part of the module.

--- python/scenicplus_core/algorithms/__init__.py
+++ python/scenicplus_core/algorithms/__init__.py
@@ -3,9 +3,13 @@ from scenicplus_core.algorithms.sort import sort
 from scenicplus_core.scenicplus_core import algorithms as spc_algorithms
 
 norm_sf = spc_algorithms.norm_sf
+rank_sums = spc_algorithms.rank_sums
+rank_sums_2d = spc_algorithms.rank_sums_2d
 
 __all__ = [
     "arg_sort",
     "norm_sf",
+    "rank_sums",
+    "rank_sums_2d",
     "sort",
 ]
