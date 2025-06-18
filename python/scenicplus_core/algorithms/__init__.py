f59d332c347d4ec0c9abf8999e3e151d7dfaee2e
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-06-18 16:38:26 +0200

    Combine arg_sort and sort submodule in one hidden submodule.
    Combine arg_sort and sort submodule in one hidden submodule
    as the actual functions inside those submodules get exported
    directly in `scenicplus_core.algorithms`.

--- python/scenicplus_core/algorithms/__init__.py
+++ python/scenicplus_core/algorithms/__init__.py
@@ -1,5 +1,4 @@
-from scenicplus_core.algorithms.arg_sort import arg_sort
-from scenicplus_core.algorithms.sort import sort
+from scenicplus_core.algorithms._sorting import arg_sort, sort
 from scenicplus_core.scenicplus_core import algorithms as spc_algorithms
 
 norm_sf = spc_algorithms.norm_sf
