f07d667bd5c5328dce2abe86495386d9457c977d
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-06-19 14:45:46 +0200

    Add `p_adjusted_bh` function from pycisTopic.
    Add `p_adjusted_bh` function from pycisTopic:
      https://github.com/aertslab/pycisTopic/blob/cc0f26e45bcca067dcd4f10aabd293ec171e1fea/src/pycisTopic/diff_features.py#L1492-L1499

--- python/scenicplus_core/algorithms/__init__.py
+++ python/scenicplus_core/algorithms/__init__.py
@@ -1,5 +1,6 @@
 from scenicplus_core.algorithms._nonzero_rows import get_nonzero_row_indices
 from scenicplus_core.algorithms._sorting import arg_sort, sort
+from scenicplus_core.algorithms._stats import p_adjust_bh
 from scenicplus_core.scenicplus_core import algorithms as spc_algorithms
 
 norm_sf = spc_algorithms.norm_sf
@@ -10,6 +11,7 @@ __all__ = [
     "arg_sort",
     "get_nonzero_row_indices",
     "norm_sf",
+    "p_adjust_bh",
     "rank_sums",
     "rank_sums_2d",
     "sort",
